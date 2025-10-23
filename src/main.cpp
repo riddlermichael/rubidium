a546eeee0477870d8a782eb1da0ccbbf7f2451f2
Michael Galuza <riddlermichael@yandex.ru>
2025-10-24 00:28:50 +0700

    finalize

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,6 @@ using std::endl;
 using namespace rb;
 using namespace rb::core;
 
-#ifndef RB_GL_FRAMEBUFFER_SRG
-	#define RB_GL_FRAMEBUFFER_SRGB 0x8DB9
-#endif
-
 int main() {
-	int x = 42;
 	cout << CompilerInfo::kThis << endl;
 }
